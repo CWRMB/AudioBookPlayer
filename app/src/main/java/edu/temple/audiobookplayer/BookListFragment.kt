635f7cf2bf629aef44793e5f71024a2095ef44b0
Michael Bennett <tun78940@temple.edu>
2022-03-21 12:44:42 -0400

    Further development of the BookListFragment.kt fragment class. Currently adding a recycler view within the fragment view. Which then I will have to add another xml layout to display the contents of each element within the recycler view.

--- app/src/main/java/edu/temple/audiobookplayer/BookListFragment.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,7 +18,6 @@ private const val BOOKS_KEY = "book_key"
  * create an instance of this fragment.
  */
 class BookListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var books: Array<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +33,46 @@ class BookListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_list, container, false) as RecyclerView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(view as RecyclerView){
+            // define attributes of view object which are defined in this scope as a recyclerView
+            // requireContext() to tell it I am insisting there is an attached context
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    class BookAdapter(_books: Array<Book>) : RecyclerView.Adapter<BookAdapter.BookViewHolder>(){
+        val books = _books
+
+        class BookViewHolder(_view: View) : RecyclerView.ViewHolder(_view){
+            val view = _view
+
+            // section for onclick listener to update our clicked object
+            init{
+
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+            // Have to change the xml layout to a recycler view
+            val layout = LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_book_list, parent, false
+            )
+
+            return BookViewHolder(layout)
+        }
+
+        override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            return books.size
+        }
+
     }
 
     companion object {
