c370a143601999fe1573afd487bed5802cb78604
Michael Bennett <tun78940@temple.edu>
2022-03-22 23:04:08 -0400

    onClick function is now fully functional. It updates our display in both landscape and portrait mode. Updated xml text styles and spacing. Display fragment is now added to backStack to allow back button press when in vertical orientation mode.

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -2,24 +2,40 @@ package edu.temple.audiobookplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // book view model to hold our livedata information to send to the fragment with onClick
+        val bookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
+
+        // send this information to our container fragment and add it to the backStack to allow back button
+        bookViewModel.getSelectedBook().observe(this){
+            if(findViewById<View>(R.id.container2) == null) {
+                supportFragmentManager.beginTransaction().replace(R.id.container1, DisplayFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        // creation of our book list from our strings.xml
         val authors = resources.getStringArray(R.array.book_authors)
         val titles = resources.getStringArray(R.array.book_names)
 
         val books: ArrayList<Book> = arrayListOf(Book("",""))
 
         for(i in authors.indices){
-            books.add(Book(authors[i], titles[i]))
+            books.add(Book(titles[i], authors[i]))
         }
 
         val my_books: BookList = BookList(books)
 
-        supportFragmentManager.beginTransaction().add(
+        // pass our book list to the main container which is a fragment adapter for recyclerview
+        supportFragmentManager.beginTransaction().replace(
             R.id.container1, BookListFragment.newInstance(my_books)).commit()
     }
 
